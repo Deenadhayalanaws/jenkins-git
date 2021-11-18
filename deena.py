b134a283b261b0889193254cb5f9421459304d69
“Deenadhayalanaws” <deenadeej@gmail.com>
2021-11-18 11:36:50 +0000

    my first commit

---
+++ deena.py
@@ -0,0 +1,12 @@
+http://18.183.230.160:8080/
+http://18.1
+83.230.160:8080/job/build%20project/build?token=1234
+git config --global user.name  “Deenadhayalanaws”
+
+git config --global user.email “deenadeej@gmail.com”
+
+git config --global core.editor vim
+
+git config --global core.compression 2
+
+git config --global diff.tool vim.diff"
